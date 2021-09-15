0c5a08cb67f70b3829a66a4695436d1915e418dd
Jonathan-0101 <jonathan.woolf.01@gmail.com>
2021-09-15 09:51:22 +0100

    Clearing working tree

--- Backend/reader.py
+++ Backend/reader.py
@@ -1,11 +1,10 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 PIR_PIN = 14
 reader = SimpleMFRC522()
-card_info = str(reader.read(timeout = 5))
+card_info = str(reader.read(timeout = 15))
 print(card_info)
 f = open("temp.txt", "wt")
 f.write(card_info)
