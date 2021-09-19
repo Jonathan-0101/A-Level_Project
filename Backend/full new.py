474d926cc033e324768c9ebf72d06b7732505b23
Jonathan-0101 <jonathan.woolf.01@gmail.com>
2021-09-19 22:49:03 +0100

    Adding new recording method  + file cleanup

--- Backend/full new.py
+++ Backend/full new.py
@@ -1,10 +1,11 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-import cv2
+from picamera import PiCamera
 import time
 from datetime import datetime
 import sqlite3
 
+camera = PiCamera()
 conn = sqlite3.connect('System.db', check_same_thread=False)
 conn.execute('''CREATE TABLE if not exists ENTRY_LOG
   (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -36,30 +37,18 @@ def unlock():
     GPIO.setup(Relay_PIN, GPIO.OUT)
     GPIO.output(Relay_PIN, GPIO.HIGH)
     print('Door unclocked')
-    time.sleep(5)
+    time.sleep(10)
+    camera.stop_recording()
+    camera.stop_preview()
     lock()
     
 def pir(pin):
-    #recording start
-    Stop_recording = False
-    cap= cv2.VideoCapture(0)
     now = datetime.now()
     dt_string = now.strftime("%d_%m_%Y %H¦%¦%S")
-    recording_title = dt_string + '.mp4'
-    width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    writer= cv2.VideoWriter(recording_title, cv2.VideoWriter_fourcc(*'DIVX'), 20, (width,height))
-    while True:
-        ret,frame= cap.read()
-        writer.write(frame)
-        cv2.imshow('frame', frame)
-        if Stop_recording == True:
-            time.sleep(10)
-            break
-    cap.release()
-    writer.release()
-    cv2.destroyAllWindows()
-    #recording end
+    recording_path = ("/home/pi/Documents/Project/Recordings/")
+    recording_title = (recording_path + dt_string + ".h264")
+    camera.start_preview()
+    camera.start_recording(recording_title)
     print('Motion Detected!')
     print("Hold a tag near the reader")
     #Reading card and waiting with timeout
@@ -68,18 +57,18 @@ def pir(pin):
     cursor=conn.execute("SELECT text FROM ID_CARDS Where ID = ?", [card_id]).fetchall()
     print(cursor)
     to_check = cursor[0]
-    if to_check[0] == card_text:
+    if to_check[0] == cursor:
         print('Authorised')
-        Stop_recording = True
         Authorised = True
         conn.execute("INSERT INTO ENTRY_LOG(Authorised, USER_ID, DateTime) VALUES (?,?,?)", [Authorised, card_id, dt_string]).lastrowid
         conn.commit()
         unlock()    
     else:
         print("not authorised")
-        time.sleep(10)
-        Stop_recording = True
+        time.sleep(15)
         Authorised = False
+        camera.stop_recording()
+        camera.stop_preview()
         conn.execute("INSERT INTO ENTRY_LOG(Authorised, DateTime) VALUES (?,?,?)", [Authorised, dt_string]).lastrowid
         conn.commit()
 
