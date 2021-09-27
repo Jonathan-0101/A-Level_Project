5ea1c25052adf177898b0ecc0012c898840ccbc1
Jonathan-0101 <jonathan.woolf.01@gmail.com>
2021-09-27 14:01:48 +0100

    Cleanup

--- Backend/Extra stuff/New.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-$ timeout 30s tail -f /var/log/pacman.log
-DATE=$(date +"%Y-%m-%d_%H%M")
-ffmpeg -f v4l2 -r 25 -s 640x480 -i /dev/video0 $DATE.avi
--- Backend/Extra stuff/camera.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-DATE=$(date + %d-%m-%y_%H%M)
-ffmpeg -f v4l2 -r 25 -s 640x480 -i /dev/video0 $DATE.avi
