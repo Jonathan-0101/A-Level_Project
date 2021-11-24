9291ba48e5866811f8c80cb0ab5c6695d4b7d45d
Jonathan-0101 <jonathan.woolf.01@gmail.com>
2021-11-24 10:05:59 +0000

    Missing staged changes

--- Backend/write card and db.py
+++ Backend/write card and db.py
@@ -9,36 +9,48 @@ def dict_factory(Cursor, row):
         d[col[0]] = row[idx]
     return
 
-conn = sqlite3.connect('System.db')
+conn = sqlite3.connect('System.db') #Connects to the Database
 conn.execute('''CREATE TABLE if not exists ID_CARDS 
   (ID INTEGER PRIMARY KEY AUTO_INCREMENT,
   Hashed_ID INTEGER,
   Username VARCHAR,
   First_name TEXT,
   Last_name TEXT,
-  Time_created DATETIME);''')
-conn.commit()
+  Time_created DATETIME);''') #Creates the dable if it does not exist
+conn.commit() #Commits it to the database
 
+#Sets up the GPIO pins and defines the reader
 GPIO.setwarnings(False)
 reader = SimpleMFRC522()
 
-while True:
-    x = input("Do you want to read or write, R for read, W for write: ")
-    if x == 'w':
-        text = input('New data:')
-        firstName = input('First name: ')
-        lastName = input('Last name: ')
-        timeCreated = datetime.now()
-        print("Now place your tag to write")
-        reader.write(text)
-        print("Written")
-        cardId, text = reader.read()
-        cardId = cardId % 1999
-        print()
-        conn.execute("INSERT INTO ID_CARDS(Hashed_ID, text, First_name, Last_name, Time_created) VALUES (?,?,?,?,?)", [cardId, text, firstName, lastName, timeCreated]).lastrowid
-        conn.commit()
-    elif x == 'r':
-        print("Hold a tag near the reader")
-        ID, text = reader.read()
-        print("ID: %s\nText: %s" % (ID, text))
-        print()
+def writeCard(): #Function for writing the card and db
+    #Inputs to get information about the card owner
+    text = input('New data:')
+    firstName = input('First name: ')
+    lastName = input('Last name: ')
+    timeCreated = datetime.now()
+    print("Now place your tag to write")
+    reader.write(text)
+    print("Written")
+    cardId, text = reader.read() #Reading the card
+    cardId = cardId % 1999 #Hashing the cardId
+    print()
+    conn.execute("INSERT INTO ID_CARDS(Hashed_ID, text, First_name, Last_name, Time_created) VALUES (?,?,?,?,?)", [cardId, text, firstName, lastName, timeCreated]).lastrowid #Writes the information to the database
+    conn.commit()
+
+def readCard(): #Function for reading the card
+    print("Hold a tag near the reader")
+    ID, text = reader.read()
+    print("ID: %s\nText: %s" % (ID, text))
+    print()
+
+def menu(): #Menu for the user to choose what they want to do
+    choice = input("Do you want to read or write, R for read, W for write: ")
+    if choice == 'w' or 'W':
+        writeCard()
+        menu()
+    elif choice == 'r' or 'R':
+        readCard()
+        menu()
+
+menu()
