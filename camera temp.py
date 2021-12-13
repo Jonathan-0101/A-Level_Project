95ee2a51ec304fd1b4751022d9b714ee8d5aada0
Jonathan-0101 <jonathan.woolf.01@gmail.com>
2021-12-13 17:46:30 +0000

    camera fix

--- camera temp.py
+++ camera temp.py
@@ -7,18 +7,22 @@ from datetime import datetime
 from mfrc522 import SimpleMFRC522
 from picamera.array import PiRGBArray
 
+# Seting up the Pins and devices connected to the Rasberry Pi
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
 PIR_PIN = 14
 reader = SimpleMFRC522()
+
 GPIO.setup(PIR_PIN, GPIO.IN) # Setup GPIO pin PIR as input
 print('Sensor initializing . . .')
 time.sleep(15) # Give sensor time to start-up, 16 seconds
 print('Active')
+
 Relay_PIN = 4
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
 
 def y():
     print("Y")
@@ -33,7 +37,7 @@ def n():
     camera.stop_preview()
     
 def main():
-    recordingPath = ("/home/pi/Project")
+    recordingPath = ("/home/pi/Project/")
     videoFileName = "test"
     recordingTitle = (recordingPath + videoFileName + ".h264")
     camera.start_preview(alpha=200)
@@ -42,5 +46,7 @@ def main():
     x = input("?: ")
     if x == "Y":
         y()
-    if x == "Y":
+    if x == "N":
         n()
+
+main()
