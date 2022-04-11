3d6426a6e4db8c20b8454065ce75b28372d199cd
Jonathan-0101 <jonathan.woolf.01@gmail.com>
2022-04-11 23:40:10 +0100

    Adding mariadb fuctionality to backend

--- backend main.py
+++ backend main.py
@@ -1,102 +1,110 @@
 # Importing requiered modules
 import os
 import time
-import sqlite3
+import mariadb
 import RPi.GPIO as GPIO
 from picamera import PiCamera
 from datetime import datetime
+from dotenv import load_dotenv
 from mfrc522 import SimpleMFRC522
 
-conn = sqlite3.connect('database.db', check_same_thread=False) # Connects to the Database
+load_dotenv()
+dbIp = os.getenv("dbIp")
+dbUserName = os.getenv('dbUserName')
+dbPassword = os.getenv('dbPassword')
 
-conn.execute('''CREATE TABLE if not exists entryLog 
-  (id INTEGER PRIMARY KEY AUTOINCREMENT,
-  authorised INTEGER,
-  userId INTEGER,
-  dateTime DATETIME)''') # Creates the tables if it does not exist
-conn.commit() # Commits to the database
+cur = mariadb.connect(host=dbIp, database='iSpy', user=dbUserName, password=dbPassword)
+
+conn = cur.cursor()
 
 # Seting up the Pins and devices connected to the Rasberry Pi
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 PIR_PIN = 14
 reader = SimpleMFRC522()
-GPIO.setup(PIR_PIN, GPIO.IN) # Setup GPIO pin PIR as input
+GPIO.setup(PIR_PIN, GPIO.IN)  # Setup GPIO pin PIR as input
 print('Sensor initializing . . .')
-time.sleep(15) # Give sensor time to start-up, 15 seconds
+time.sleep(15)  # Give sensor time to start-up, 15 seconds
 print('Active')
 Relay_PIN = 4
+
 
 def cameraStop(fileName, camera):
     time.sleep(10)
     camera.stop_recording()
     camera.close()
 
-def lock(): # Function for locking the door
+
+def lock():  # Function for locking the door
     GPIO.setup(Relay_PIN, GPIO.OUT)
     GPIO.output(Relay_PIN, GPIO.LOW)
     print('Door locked')
-    
-def unlock(): # Function for unlocking the door and stopping the recording
+
+
+def unlock():  # Function for unlocking the door and stopping the recording
     GPIO.setup(Relay_PIN, GPIO.OUT)
     GPIO.output(Relay_PIN, GPIO.HIGH)
     print('Door unlocked')
     time.sleep(30)
 
-def lockMain(fileName, camera): # Function for locking the door
+
+def lockMain(fileName, camera):  # Function for locking the door
     GPIO.setup(Relay_PIN, GPIO.OUT)
     GPIO.output(Relay_PIN, GPIO.LOW)
     print('Door locked')
     cameraStop(fileName, camera)
 
 
-def unlockMain(fileName, camera): # Function for unlocking the door and stopping the recording
+def unlockMain(fileName, camera):  # Function for unlocking the door and stopping the recording
     GPIO.setup(Relay_PIN, GPIO.OUT)
     GPIO.output(Relay_PIN, GPIO.HIGH)
     time.sleep(10)
     print('Door unlocked')
     lockMain(fileName, camera)
 
-def pir(pin): # Function for running the events when motion is detected
-    now = datetime.now() # Getting current date and time
-    dateTime = now
-    cursor = conn.execute("SELECT * FROM entryLog").fetchall()
 
-    #Naming the video file in relation to the event id
+def pir(pin):  # Function for running the events when motion is detected
+    now = datetime.now()  # Getting current date and time
+    dateTime = now
+    cursor = conn.execute("SELECT * FROM entryLog")
+    cursor = conn.fetchall()
+
+    # Naming the video file in relation to the event id
     if len(cursor) == 0:
         videoFileName = "1"
 
     else:
         videoFileName = str(len(cursor)+1)
 
-    #Getting path to the file as will differ per computer
+    # Getting path to the file as will differ per computer
     path = os.getcwd()
-    #Add the requiered video to the end of the path
+    # Add the requiered video to the end of the path
     fileName = (path + "/Recordings/" + videoFileName + ".h264")
-    camera = PiCamera() # Setting the camera that will be used
+    camera = PiCamera()  # Setting the camera that will be used
     camera.resolution = (1920, 1080)
-    camera.framerate = 25 # Sets the frame rate of the camera
+    camera.framerate = 25  # Sets the frame rate of the camera
     camera.start_preview(alpha=200)
-    time.sleep(0.1) # Delay for camera preview to start up
-    camera.start_recording(fileName) # Starts the recording
+    time.sleep(0.1)  # Delay for camera preview to start up
+    camera.start_recording(fileName)  # Starts the recording
     print('Motion Detected!')
     # Setting default values for cardId and cardName if they do not get written
     cardId = None
     cardName = None
-    cardId, cardName = reader.read(timeout=20) # Reading the card waiting for 20 seconds if no card is scanned
-    cardId = cardId # Hashing the cardId
+    cardId, cardName = reader.read(timeout=20)  # Reading the card waiting for 20 seconds if no card is scanned
+    cardId = cardId  # Hashing the cardId
 
     # Checks if the card is authorised
-    cursor = conn.execute("SELECT text FROM idCards Where cardId = ? and cardName = ? and active = 1", [cardId, cardName]).fetchall()
+    cursor = conn.execute("SELECT text FROM idCards Where cardId = ? and cardName = ? and active = 1", (cardId, cardName,))
+    cursor = conn.fetchall()
     cardCheck = cursor[0]
 
     if len(cardCheck) == 1:
         print('Authorised')
         authorised = True
         # Adds the event into the entry log
-        conn.execute("INSERT INTO entryLog(authorised, userId, dateTime) VALUES (?,?,?)", [authorised, cardId, dateTime])
-        conn.commit()
-        unlock(fileName, camera) # Calls the function to unlock the door
+        conn.execute("INSERT INTO entryLog(authorised, userId, dateTime) VALUES (?,?,?)", (authorised, cardId, dateTime,))
+        cur.commit()
+        unlock(fileName, camera)  # Calls the function to unlock the door
 
     else:
         print("Not authorised")
@@ -105,17 +113,18 @@ def pir(pin): # Function for running the events when motion is detected
         camera.stop_recording()
         camera.stop_preview()
         # Adds the event to the events log
-        conn.execute("INSERT INTO entryLog(authorised, dateTime) VALUES (?,?)", [authorised, dateTime])
-        conn.commit()
+        conn.execute("INSERT INTO entryLog(authorised, dateTime) VALUES (?,?)", (authorised, dateTime,))
+        cur.commit()
 
-lock() # Calling the lock function on statrup to lock the door incase there is a power loss
+
+lock()  # Calling the lock function on statrup to lock the door incase there is a power loss
 camera = PiCamera()
 camera.close()
 
-GPIO.add_event_detect(14, GPIO.FALLING, callback=pir, bouncetime=100) # Checks for motion
+GPIO.add_event_detect(14, GPIO.FALLING, callback=pir, bouncetime=100)  # Checks for motion
 
 try:
-    while True: # Loops the check for motion
+    while True:  # Loops the check for motion
         time.sleep(0.5)
 
 except KeyboardInterrupt:
--- lock checker.py
+++ lock checker.py
@@ -1,12 +1,22 @@
+import os
 import time
-import sqlite3
+import mariadb
 import RPi.GPIO as GPIO
+from dotenv import load_dotenv
 
 Relay_PIN = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-conn = sqlite3.connect("database.db", check_same_thread=False)
+load_dotenv()
+dbIp = os.getenv("dbIp")
+dbUserName = os.getenv('dbUserName')
+dbPassword = os.getenv('dbPassword')
+
+cur = mariadb.connect(host=dbIp, database='iSpy', user=dbUserName, password=dbPassword)
+
+conn = cur.cursor()
+
 
 def unlock():
     # Function for unlocking the door
@@ -16,6 +26,7 @@ def unlock():
     # Timeout to give user oppotunity to enter door
     time.sleep(30)
 
+
 def lock():
     # Function for locking the door
     GPIO.setup(Relay_PIN, GPIO.OUT)
@@ -23,13 +34,15 @@ def lock():
     print("Door locked")
     # Resseting value in db so that door can be unlocked again
     conn.execute("Update doorStatus set lockStatus = 0")
-    conn.commit()
+    cur.commit()
+
 
 def main():
     # Creates while loop to check if there is an update to the database
     while True:
         # Retriveing the doorStatus value from the table
-        cursor = conn.execute("SELECT * FROM door Status").fetchall()
+        cursor = conn.execute("SELECT * FROM door Status")
+        cursor = conn.fetall()
         # Checks if the value is equal to 1 (whether it should open)
         if cursor[0][0] == 1:
             # Calls unlock function and then locking function
@@ -41,5 +54,6 @@ def main():
             # Adds a time delay before the table is checked again
             time.sleep(5)
 
-lock() # Ensures door is locked when the program starts up
+
+lock()  # Ensures door is locked when the program starts up
 main()
